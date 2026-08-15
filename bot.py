40361f0ff2ceb9b3b9e12a15b5e34aeefb2dd637
botgrasa-67 <felipecabre2025@gmail.com>
2026-08-15 19:28:38 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ async def aura(ctx, usuario: discord.Member = None):
 # Comando 2: Juanfer con texto e imagen directa
 @bot.command()
 async def juanfer(ctx):
-    await ctx.send("🎩 **¡Que la gente crea!**\nhttps://i.postimg.cc/L5v8v2XN/juanfer.gif")
+    await ctx.send("🎩 **¡Que la gente crea!**https://media1.tenor.com/m/b1hLitFQ5jMAAAAd/quintero-river-plate.gif")
 
 # Respuestas automáticas por palabras clave
 @bot.event
